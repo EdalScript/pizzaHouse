1137464d762258c85d42749578657a6a2db1fa0f
Edgar Torres <edalscript@gmail.com>
2022-03-13 20:19:34 +0300

    styling

--- templates/header.php
+++ templates/header.php
@@ -6,20 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <style type="text/css">
-        .brand{
+        .brand {
             background: #cbb89c !important;
         }
+
         .brand-text {
-            color:#cbb89c !important;
+            color: #cbb89c !important;
         }
-        </style>
+
+        form {
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+        }
+    </style>
 </head>
+
 <body class="grey lighten-4">
     <nav class="white z-depth-0">
         <div class="container">
-             <a href="#" class="center brand-logo brand-text">Pizza House</a>
-                 <ul id="nav-mobile" class="right hide-on-small-and-down">
-                   <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
-                 </ul>
+            <a href="#" class="center brand-logo brand-text">Pizza House</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+            </ul>
         </div>
     </nav>
