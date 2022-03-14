57a1f06b46a0e02474e49d7b15e6cff3a0a68bfd
Edgar Torres <edalscript@gmail.com>
2022-03-14 17:30:06 +0300

    error handling properties

--- add.php
+++ add.php
@@ -1,33 +1,41 @@
 <?php
-    if(isset($_POST['submit'])){
-        echo htmlspecialchars($_POST['email']);
-        echo htmlspecialchars($_POST['title']);
-        echo htmlspecialchars($_POST['ingredients']);
-    }
-    if(empty($_POST['email'])){
-        echo 'An email is required <br />';
-    }else {
+
+$email = $title = $ingredients = '';
+$errors = array('email' => '', 'title' => '', 'ingredients' => '');
+
+
+if (isset($_POST['submit'])) {
+
+    if (empty($_POST['email'])) {
+        $errors['email'] = 'An email is required <br />';
+    } else {
         $email = $_POST['email'];
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            echo 'Email must be a valid email address';
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = 'Email must be a valid email address';
         }
     }
-    if(empty($_POST['title'])){
-        echo 'A title is required <br />';
+    if (empty($_POST['title'])) {
+        $errors['title'] = 'A title is required <br />';
     } else {
         $title = $_POST['title'];
-        if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-            echo 'Title must be letters and spaces only';
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+            $errors['title'] = 'Title must be letters and spaces only';
         }
     }
-    if(empty($_POST['ingredients'])){
-        echo 'At least one ingredient is required <br />';
+    if (empty($_POST['ingredients'])) {
+        $errors['ingredients'] = 'At least one ingredient is required <br />';
     } else {
         $ingredients = $_POST['ingredients'];
-        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){ // makes sure it's comma separated
-            echo 'Ingredients must be a comma separated list';
+        if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) { // makes sure it's comma separated
+            $errors['ingredients'] = 'Ingredients must be a comma separated list';
         }
     }
+    if(array_filter($errors)){
+       // echo 'Errors in the form';
+    } else {
+        header('Location: index.php');
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -38,11 +46,14 @@
     <h4 class="center">Add a Pizza</h4>
     <form class="white" action="add.php" method="POST">
         <label>Your Email: </label>
-        <input type="text" name="email">
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+        <div class="red-text"><?php echo $errors['email']; ?></div>
         <label>Pizza Title: </label>
-        <input type="text" name="title">
+        <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+        <div class="red-text"><?php echo $errors['title']; ?></div>
         <label>Ingredients (comma separated): </label>
-        <input type="text" name="ingredients">
+        <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+        <div class="red-text"><?php echo $errors['ingredients']; ?></div>
         <div class="center">
             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
@@ -50,4 +61,5 @@
 </section>
 
 <?php include('templates/footer.php'); ?>
+
 </html>
